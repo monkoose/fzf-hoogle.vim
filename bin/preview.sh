83f6d0d05dc522405f29bbf810ad02368b05838b
monkoose <monkoose@protonmail.com>
2019-12-10 01:45:31 +0200

    Refactor preview.sh

--- bin/preview.sh
+++ bin/preview.sh
@@ -5,28 +5,10 @@ if [ ! -r "$1" ]; then
   exit 1
 fi
 
-dim=$(tput setaf 8)
-blue=$(tput setaf 4)
-green=$(tput setaf 2)
-bold=$(tput setaf 11)
-reset=$(tput sgr0)
-
+# json item from temporary file with help of fzf {n} ($3)
 LINE="sed -n $(($3+1))p $1"
 
-PACKAGE="$LINE | jq -r '.package.name // empty'"
-if [[ ! -z "$PACKAGE" ]]
-then
-  PACKAGE="$PACKAGE | sed 's/^/$blue/; s/$/$reset/'"
-fi
-
-MODULE="$LINE | jq -r '.module.name // empty'"
-if [[ ! -z "$MODULE" ]]
-then
-  MODULE="$MODULE | sed 's/^/$green/; s/$/$reset/'"
-fi
-
-ITEM="$LINE | jq -r '.item' | sed 's/^/$bold/; s/$/$reset\n/' "
-
-DOCS="$LINE | jq -r '.docs' | sed 's/<pre>/$dim/g; s/<\/pre>/$reset/g; s/<[^>]*>//g; s/&amp;/\&/g; s/&gt;/>/g; s/&lt;/</g'"
-
-eval "$PACKAGE;$MODULE;$ITEM;$DOCS"
+eval "$LINE | jq -r '.package.name // empty' | sed 's/^/$(tput setaf 4)/; s/$/$(tput sgr0)/'"
+eval "$LINE | jq -r '.module.name // empty' | sed 's/^/$(tput setaf 2)/; s/$/$(tput sgr0)/'"
+eval "$LINE | jq -r '.item' | sed 's/^/$(tput setaf 11)/; s/$/$(tput sgr0)\n/' "
+eval "$LINE | jq -r '.docs' | sed 's/<pre>/$(tput setaf 8)/g; s/<\/pre>/$(tput sgr0)/g; s/<[^>]*>//g; s/&amp;/\&/g; s/&gt;/>/g; s/&lt;/</g'"
